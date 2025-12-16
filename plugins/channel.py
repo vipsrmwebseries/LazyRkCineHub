9336c36de508d22dbd1496389d7d45e89801be35
owajbhai <owajshaikhrcm@gmail.com>
2025-12-16 22:33:38 +0530

    Fix title formatting in full_caption

--- plugins/channel.py
+++ plugins/channel.py
@@ -130,7 +130,7 @@ async def send_movie_update(bot, file_name, caption):
         
         # Title Fix: টাইটেল র‍্যাপিং (New Feature)
         title_lines = textwrap.wrap(title, width=32)
-        full_caption += f"│ 📂 𝐓𝐢𝐭𝐥𝐞: <b>{title_lines[0]}</b>\n"
+        full_caption += f"│ 📂 𝐓𝐢𝐭𝐥𝐞: <code>{title_line[0]}</code>\n"
         for line in title_lines[1:]:
              full_caption += f"│        <b>{line}</b>\n"
         
