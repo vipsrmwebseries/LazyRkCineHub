694f9a9846f87d792feddab5133e643d35fe2443
owajbhai <owajshaikhrcm@gmail.com>
2025-12-16 13:55:23 +0530

    Fix formatting in IMDB_TEMPLATE_TXT

--- Script.py
+++ Script.py
@@ -424,7 +424,7 @@ Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
 """
     
     IMDB_TEMPLATE_TXT = """
-<b>🏷 Title</b>: <a href={url}>{title}</a>
+<b>🏷 Title</b>: </i></b> <code>{}</code>
 🎭 Genres: {genres}
 � Year: <a href={url}/releaseinfo>{year}</a>
 🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
